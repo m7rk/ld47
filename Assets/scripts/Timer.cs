42aa0fedbcb48ba8be3542e56d7af0397995918b
m7rk <>
2020-10-03 08:43:20 -0700

    add clock code

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -16,9 +16,6 @@ public class Timer : MonoBehaviour
     // Text countdown
     public Text timeRemainDisplay;
 
-    // Allows code to know when rooms change
-    public RoomTransitionListener RoomTransitionListener;
-
     
     // Start is called before the first frame update
     void Start()
