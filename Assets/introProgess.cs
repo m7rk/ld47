4ce15dd86085866371e3359a65601c7a802004c5
JomoOval <jomovaug@gmail.com>
2020-10-03 14:05:01 -0700

    Added some test images for an intro
    Used a test scene i already had. Also added a script to make the intro images change upon clicking (still a work in progress)

---
+++ Assets/introProgess.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introProgess : MonoBehaviour
+{
+    public Sprite intro1, intro2, intro3, menu;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checking for mouse imput
+		if (input.GetMouseButtonDown(0))
+		{
+			//somehow change the sprite
+			this.GetComponent<SpriteRenderer>().sprite = intro2;
+		}
+    }
+}
