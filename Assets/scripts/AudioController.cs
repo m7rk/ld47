00f38f1878a30ac265f8659fd71620105498338e
Jacob <jacobmcalpin@gmail.com>
2020-10-03 20:17:41 -0400

    AudioController
    Class AudioController has a function changed changeTrack which takes as arguments 2 audioSources. The first audiosource fades to zero volume over 5 seconds (can easily be adjusted on a public variable in AudioController). The second Audiosource starts playing at zero volume and gradually fades up to full volume over 5 seconds. (With fades in both cases being done by class FadeAudioSource.)
    
    Depending on how the rest of the code and scene are setup, it may be possible to determine the currently playing track with code and reduce the function down to one argument. However, I think I would need to see more of the game in action before that is possible.

---
+++ Assets/scripts/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    public float fadeTime = 5f;
+    
+    public void changeTrack(AudioSource audioOut, AudioSource audioIn)
+    {
+        //turn off current audio
+        StartCoroutine(FadeAudioSource.StartFade(audioOut, fadeTime, 0f));
+
+        //turn on new audio
+        audioIn.volume = 0f;
+        audioIn.Play();
+        StartCoroutine(FadeAudioSource.StartFade(audioIn, fadeTime, 1f));
+    }
+}
