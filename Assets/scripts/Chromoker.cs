e3f11b7b9ea6c6f7706c095c4b31cb35bb46e634
m7rk <>
2020-10-05 11:46:42 -0700

    Add boss ai

---
+++ Assets/scripts/Chromoker.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chromoker : Enemy
+{
+    public Animator animator;
+
+    private  Vector3 origin;
+
+    public RoomManager rtl;
+
+    float t1, t2;
+
+    float t1delt = 0.4f;
+    float t2delt = 5f;
+
+    float patternReset = 15f;
+
+    public void Awake()
+    {
+        currentHP = maxHealth();
+        origin = this.transform.position;
+        rtl = FindObjectOfType<RoomManager>();
+
+    }
+
+    public override void hurt()
+    {
+        playHurtSound();
+        currentHP--;
+        if (currentHP == 0)
+        {
+            animator.SetTrigger("die");
+            GetComponent<BoxCollider2D>().enabled = false;
+            nextLevel();
+        }
+    }
+
+    // fuckkkkkkkkkkkkkkkk
+    public void fireOncePerSec()
+    {
+        if ((int)((3 * patternReset) % 2) != (int)(((3 * (patternReset + Time.deltaTime))) % 2))
+        {
+            int dy = -1;
+            for (int dx = -1; dx != 2; ++dx)
+            {
+                if (dx == 0 && dy == 0)
+                {
+                    continue;
+                }
+                makeProjectile(new Vector2(dx, dy), "PhantomProjectile", Vector3.zero);
+                animator.SetBool("shoot", true);
+            }
+        }
+    }
+
+    public void Update()
+    {
+        //if (!rtl.inRoom(this.transform.position) || currentHP <= 0)
+        //{
+        //    return;
+        //}
+
+        fireOncePerSec();
+        patternReset -= Time.deltaTime;
+
+        if(patternReset < 0)
+        {
+            t1delt = Random.Range(0.2f, 0.4f);
+            t2delt = Random.Range(3f, 5f);
+            patternReset = 15f;
+        }
+
+        t1 += t1delt * Time.deltaTime;
+        t2 += t2delt * Time.deltaTime;
+
+
+
+        this.transform.position = origin + new Vector3(3f * Mathf.Cos(t1), 0.25f * Mathf.Sin(t1 * 1.1f)) + new Vector3(Mathf.Sin(t2 ), 0.1f * Mathf.Cos(t2 * 1.1f));
+    }
+
+
+    public override int maxHealth()
+    {
+        return 3;
+    }
+}
---
+++ Assets/scripts/TheForgotten.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheForgotten : Enemy
+{
+    public Animator animator;
+
+    public float cooldownTime; //timer to reactivate teleports
+    private const float minimumTeleport = 8f; //the fastest two teleports can be
+    private const float maximumTeleport = 14f; //the slowest two teleports can be
+
+    public float spawnTime = 1.5f;
+
+    float originalX; //original position of boss, used to keep boss in bounds
+    float originalY; //original position of boss, used to keep boss in bounds
+    float originalZ; //original position of boss, used to keep boss in bounds
+
+    int randX; //distance to move in X
+    int randY; //distance to move in Y
+
+
+    public RoomManager rtl;
+
+    public void Awake()
+    {
+        currentHP = maxHealth();
+        rtl = FindObjectOfType<RoomManager>();
+    }
+
+    void Start()
+    {
+        originalX = this.transform.position.x;
+        originalY = this.transform.position.y;
+        originalZ = this.transform.position.z;
+
+        cooldownTime = 3;
+    }
+
+    public void fireOncePerSec()
+    {
+        if ((int)((2*cooldownTime) % 2) != (int)(((2 * cooldownTime + Time.deltaTime)) % 2))
+        {
+            for (int dx = -1; dx != 2; ++dx)
+            {
+                for (int dy = -1; dy != 2; ++dy)
+                {
+                    if (dx == 0 && dy == 0)
+                    {
+                        continue;
+                    }
+                    makeProjectile(new Vector2(dx, dy), "PhantomProjectile", Vector3.zero);
+                    animator.SetBool("attack", true);
+                }
+            }
+        }
+    }
+
+
+    public void Update()
+    {
+        if (!rtl.inRoom(this.transform.position) || currentHP <= 0)
+        {
+            return;
+
+        }
+        cooldownTime -= Time.deltaTime;
+        spawnTime -= Time.deltaTime;
+
+        if(spawnTime > 0f)
+        {
+            return;
+        }
+
+        if (cooldownTime > 4)
+        {
+            fireOncePerSec();
+        }
+
+        if (cooldownTime < 2f)
+        {
+            animator.SetBool("teleporting", true);
+        }
+
+        if (cooldownTime < 0)
+        {
+            randX = Random.Range(-6, 6);
+            randY = Random.Range(-3, 3);
+            this.transform.position = new Vector3((originalX + randX), (originalY + randY), originalZ);
+
+            cooldownTime = Random.Range(minimumTeleport, maximumTeleport);
+            animator.SetBool("teleporting", false);
+            spawnTime = 1.5f;
+
+        }
+    }
+
+    public override void hurt()
+    {
+        playHurtSound();
+        currentHP--;
+        if (currentHP == 0)
+        {
+            animator.SetTrigger("die");
+            GetComponent<BoxCollider2D>().enabled = false;
+            nextLevel();
+        }
+    }
+
+    public override int maxHealth()
+    {
+        return 20;
+    }
+}
