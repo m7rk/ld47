7399e82a17bdccbfe2df2cfe9ac280c4fae5c15d
JomoOval <jomovaug@gmail.com>
2020-10-05 02:41:15 -0700

    Sound Slider fully working
    Sound slider now works logrithmatically.
    Menu menu also has music and the play button now plays the starting music

--- Assets/Menu/OptionsMenu.cs
+++ Assets/Menu/OptionsMenu.cs
@@ -9,6 +9,6 @@ public class OptionsMenu : MonoBehaviour
 	
     public void SetVolume(float volume)
 	{
-		audioMixer.SetFloat("volume", volume);
+		audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
 	}
 }
