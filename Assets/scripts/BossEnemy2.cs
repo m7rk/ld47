f095618af228f08a3bafc58818e6a4b47fa5abd1
Jacob <jacobmcalpin@gmail.com>
2020-10-05 12:28:15 -0400

    Boss script update

--- Assets/scripts/BossEnemy2.cs
+++ Assets/scripts/BossEnemy2.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
-public class JacobTestScript : MonoBehaviour
+public class BossEnemy2 : Enemy
 {
+    public Animator animator;
+
     public float cooldownTime; //timer to reactivate teleports
     public float minimumTeleport = 1f; //the fastest two teleports can be
     public float maximumTeleport = 3f; //the slowest two teleports can be
@@ -16,7 +17,11 @@ public class JacobTestScript : MonoBehaviour
     int randX; //distance to move in X
     int randY; //distance to move in Y
 
-    // Start is called before the first frame update
+    public void Awake()
+    {
+        currentHP = maxHealth();
+    }
+
     void Start()
     {
         originalX = this.transform.position.x;
@@ -26,24 +31,34 @@ public class JacobTestScript : MonoBehaviour
         cooldownTime = Random.Range(minimumTeleport, maximumTeleport);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        cooldownTime -= Time.deltaTime; 
+        cooldownTime -= Time.deltaTime;
 
         if (cooldownTime < 0)
         {
             randX = Random.Range(0, 10);
             randY = Random.Range(0, 5);
             this.transform.position = new Vector3((originalX + randX), (originalY + randY), originalZ);
+            //an animation might look cool here, probably not enough time to implement I guess
             cooldownTime = Random.Range(minimumTeleport, maximumTeleport);
-            Debug.Log(cooldownTime);
         }
+    }
 
-        // quick script to add stuff on button
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-            //do something
-        //}        
+    public override void hurt()
+    {
+        playHurtSound();
+        currentHP--;
+        if (currentHP == 0)
+        {
+            animator.SetTrigger("die");
+            GetComponent<BoxCollider2D>().enabled = false;
+            nextLevel();
+        }
+    }
+
+    public override int maxHealth()
+    {
+        return 3;
     }
 }
